2271b393d9b727bb742cf3ca42968022d7fc3da3
chiradenuwan <chiranjayadenuwan@gmail.com>
2022-07-02 12:57:46 +0530

    create gallery dto

---
+++ projects/demo/src/app/modals/gallery.spec.ts
@@ -0,0 +1,7 @@
+import { Gallery } from './gallery';
+
+describe('Gallery', () => {
+  it('should create an instance', () => {
+    expect(new Gallery()).toBeTruthy();
+  });
+});
---
+++ projects/demo/src/app/modals/gallery.ts
@@ -0,0 +1,9 @@
+export class Gallery {
+  public constructor(
+    public title: string,
+    public description: string,
+    public type: string,
+    public url: string,
+  ) {
+  }
+}
