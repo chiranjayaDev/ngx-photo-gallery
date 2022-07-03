8ec10ee3a07d86e90a8294764fabe20ef26af8b3
chiradenuwan <chiranjayadenuwan@gmail.com>
2022-07-03 21:03:56 +0530

    add ngx-photo-gallery to app component

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -7,4 +7,97 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+  gallery = [
+    {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://d3nn873nee648n.cloudfront.net/HomeImages/Concept-and-Ideas.jpg'
+    }, {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://images.pexels.com/photos/1172253/pexels-photo-1172253.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    }, {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://loveshayariimages.in/wp-content/uploads/2022/05/New-Sad-Alone-Whatsapp-DP-Profile-Images-pics-free-download.jpg'
+    },
+    {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://www.bhaktiphotos.com/wp-content/uploads/2018/04/Hindu-Shiva-God-Wallpaper-Free-Download.jpg'
+    },
+    {
+      title: '',
+      description: '',
+      type: 'video',
+      url: 'https://i.pinimg.com/originals/30/05/c7/3005c7bf04e9cd6c60f6fff058062052.gif'
+    },
+    {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://www.gettyimages.com/gi-resources/images/500px/983703508.jpg'
+    }, {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80'
+    },
+    {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg'
+    },
+    {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://loveshayariimages.in/wp-content/uploads/2021/10/2021-Peaceful-Whatsapp-Dp-Images-1.jpg'
+    }, {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://www.w3schools.com/howto/img_forest.jpg'
+    },
+    {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://i.pinimg.com/474x/01/88/dc/0188dc41881e0e410b5375cdead5f49a.jpg'
+    },
+    {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://pga-tour-res.cloudinary.com/image/upload/c_fill,dpr_3.0,f_auto,g_center,h_393,q_auto,w_713/v1/pgatour/editorial/2022/04/17/fleetwood-1694-patricksmith.jpg'
+    }, {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://www.whatsappimages.in/wp-content/uploads/2021/06/HD-top-Sad-Boy-DP-for-Whatsapp-Pics-Images-Free-Download.jpg'
+    },
+    {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://www.akc.org/wp-content/uploads/2015/10/puppy-800x534.jpg'
+    },
+    {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://images.squarespace-cdn.com/content/v1/592d36a159cc6892f618c001/1542076238903-76ACUOWUN54IXFY7PN3J/happy-golden-retriever-puppy-picture-id521697453.jpg?format=2500w'
+    },
+    {
+      title: '',
+      description: '',
+      type: 'photo',
+      url: 'https://www.rd.com/wp-content/uploads/2020/05/GettyImages-1153012691.jpg'
+    }
+  ]
 }
--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {NgxPhotoGalleryModule} from "../../../ngx-photo-gallery/src/lib/ngx-photo-gallery.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxPhotoGalleryModule
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
